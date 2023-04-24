fd52a998e1c9a5ffb158e24bbac9d879da482c7f
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-04-24 11:14:37 -0300

    start install

--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div>
-      <h1>Comfy Sloth</h1>
-    </div>
-  );
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Navbar, Sidebar, Footer } from './components';
+
+function App() {
+  return (
+    <div>
+      <h1>Comfy Sloth</h1>
+    </div>
+  );
+}
+
+export default App;
