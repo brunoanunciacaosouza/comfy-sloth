d4e37b04d6ee8427f838b5d623312f08ce210ec7
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-04-24 16:16:06 -0300

    fetch products

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -18,6 +18,26 @@ const products_reducer = (state, action) => {
     return { ...state, isSidebarOpen: false };
   }
 
+  if (action.type === GET_PRODUCTS_BEGIN) {
+    return { ...state, products_loading: true };
+  }
+
+  if (action.type === GET_PRODUCTS_SUCCESS) {
+    const featured_products = action.payload.filter(
+      (product) => product.featured
+    );
+    return {
+      ...state,
+      products: action.payload,
+      featured_products,
+      products_loading: false,
+    };
+  }
+
+  if (action.type === GET_PRODUCTS_ERROR) {
+    return { ...state, products_loading: false, products_error: true };
+  }
+
   throw new Error(`No Matching "${action.type}" - action type`);
 };
 
