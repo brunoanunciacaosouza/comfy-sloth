6f6a12f5a1823ac0a25d271984b01c8cfbd5a97b
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-04-25 13:56:55 -0300

    filters price

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -108,6 +108,18 @@ const Filters = () => {
               })}
             </div>
           </div>
+          <div className='form-control'>
+            <h5>price</h5>
+            <p className='price'>{formatPrice(price)}</p>
+            <input
+              type='range'
+              name='price'
+              onChange={updateFilters}
+              min={min_price}
+              max={max_price}
+              value={price}
+            />
+          </div>
         </form>
       </div>
     </Wrapper>
