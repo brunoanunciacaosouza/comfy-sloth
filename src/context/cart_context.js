b0982e3b405365e25e559d517366397bf5e650d6
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-04-26 10:52:45 -0300

    cart totals

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -21,7 +21,7 @@ const initialState = {
   cart: getLocalStorage(),
   total_items: 0,
   total_amount: 0,
-  shipping_free: 534,
+  shipping_fee: 534,
 };
 
 const CartContext = React.createContext();
